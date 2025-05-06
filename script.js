c368715ed863b0e18cb07274d8ede418156f8b72
d3c44 <figueroadaniel749@gmail.com>
2025-05-06 15:23:05 -0400

    agregaron fechas para la tabla

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const students=[];
 document.getElementById("studentForm").addEventListener("submit",function (e){
     e.preventDefault();
-    const name=document.getElementById("name").ariaValueMax.trim();
-    const lastName=document.getElementById("lastName").ariaValueMax.trim();
+    const name=document.getElementById("name").value.trim();
+    const lastName=document.getElementById("lastName").value.trim();
+    const fecha=document.getElementById("fecha").value.trim();
     const grade=parseFloat(document.getElementById("grade").value);
 
-    if(!name || !lastName || isNaN(grade)|| grade<1 || grade>7){
+    if(!name || !lastName ||!fecha || isNaN(grade)|| grade<1 || grade>7){
         alert("Error de datos")
         return
     }
@@ -13,9 +14,15 @@ document.getElementById("studentForm").addEventListener("submit",function (e){
 
 
 
-    const student={name,lastName,grade}
+    const student={name,lastName,fecha,grade}
 
     students.push(student)
+
+    console.log(student)
+    // console.log(students)
+    addStudentToTable(student)
+
+ñ
 
 
 
@@ -23,3 +30,14 @@ document.getElementById("studentForm").addEventListener("submit",function (e){
 
 
 });
+const tableBody=document.querySelector("#studentsTabla tbody");
+function addStudentToTable(student){
+    const row=document.createElement("tr")
+    row.innerHTML= 
+    `<td>${student.name}</td>
+    <td>${student.lastName}</td>
+     <td>${student.fecha}</td>
+    <td>${student.grade}</td>`
+tableBody.appendChild(row);
+
+}
